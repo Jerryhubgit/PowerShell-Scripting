ef30ef94b2a99915ff5dbfe110769a4d9b971b9e
Jerry <chimobijeremaih@gmail.com>
2026-02-27 14:08:34 +0100

    pipline

--- shell.ps1
+++ shell.ps1
@@ -1,14 +1,1 @@
-
-
-$employees = @(
-    [PSCustomObject]@{ Name = 'Bob'; Age = 45; Role = 'Site reliabilty engineer' }
-    [PSCustomObject]@{ Name = 'shaw'; Age = 56; Role = 'DevSec Ops' }
-    [PSCustomObject]@{ Name = 'Kayode'; Age = 26; Role = 'Cybersecurity' }
-    [PSCustomObject]@{ Name = 'Michelle'; Age = 23; Role = 'Real Estate Personell' }
-    [PSCustomObject]@{ Name = 'Merlin '; Age = 156; Role = 'Magician' }
-    
-)
-
-foreach($object in $employees){
-    "$($object.Name) is a $($object.Role)"
-}
+get-service | where-object { $_.status -eq 'running'} | select-object name , DisplayName
