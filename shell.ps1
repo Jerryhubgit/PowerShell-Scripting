d5ffaed43787f3033d7dcdfbc4f9b5976be3fad3
Jerry <chimobijeremaih@gmail.com>
2026-02-27 13:16:50 +0100

    list of custom objects

--- shell.ps1
+++ shell.ps1
@@ -1,3 +1,14 @@
-$numbers = 1,2,2,3,4
 
-$numbers[3]
+
+$employees = @(
+    [PSCustomObject]@{ Name = 'Bob'; Age = 45; Role = 'Site reliabilty engineer' }
+    [PSCustomObject]@{ Name = 'shaw'; Age = 56; Role = 'DevSec Ops' }
+    [PSCustomObject]@{ Name = 'Kayode'; Age = 26; Role = 'Cybersecurity' }
+    [PSCustomObject]@{ Name = 'Michelle'; Age = 23; Role = 'Real Estate Personell' }
+    [PSCustomObject]@{ Name = 'Merlin '; Age = 156; Role = 'Magician' }
+    
+)
+
+foreach($object in $employees){
+    "$($object.Name) is a $($object.Role)"
+}
